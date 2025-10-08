c69839a32e4b0174d23455073c4e6526a6d5af8a
OHM <ohm.ratchaphon@gmail.com>
2025-10-09 00:01:14 +0700

    feat: delete toastprovider

--- apps/web/app/layout.js
+++ apps/web/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ToastProvider } from "@/components/layout/ToastProvider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -14,9 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
-        <ToastProvider>{children}</ToastProvider>
-      </body>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
